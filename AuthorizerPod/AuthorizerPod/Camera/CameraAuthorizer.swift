fceab54eedbb8dca3d558e16dc46667e58d586f0
Yeung Mon <monwingyeung@gmail.com>
2017-05-19 17:05:46 +0800

    调整目录结构

---
+++ AuthorizerPod/AuthorizerPod/Camera/CameraAuthorizer.swift
@@ -0,0 +1,14 @@
+//
+//  CameraAuthorizer.swift
+//  AuthorizerPod
+//
+//  Created by Mon on 19/05/2017.
+//  Copyright © 2017 wenyongyang. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraAuthorizer: NSObject {
+
+}
